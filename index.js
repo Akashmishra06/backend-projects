cae3fdcb1bce7e4bb73dcf62a4c84dd1f4d12b10
Akash Mishra <167092640+Akashmishra06@users.noreply.github.com>
2024-05-09 14:19:14 +0000

    basicsOfExpress

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("hello")
+
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res) => {
+    res.send("twitter.com")
+})
+
+app.get('/login', (req, res) =>{
+    res.send("username")
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
